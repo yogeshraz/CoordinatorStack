c3b661254575fb33de91e100d920767c33d12d21
Yogesh Raj <yogeshraz@hotmail.com>
2025-03-26 13:54:43 +0530

    Initial Commit

---
+++ CoordinatorStackDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoordinatorStackDemo
+//
+//  Created by Yogesh Raj on 26/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoordinatorStackDemo/CoordinatorStackDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorStackDemoApp.swift
+//  CoordinatorStackDemo
+//
+//  Created by Yogesh Raj on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorStackDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
